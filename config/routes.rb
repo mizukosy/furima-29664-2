8c2e4848250b00e97aed76716cc783db263b6a4c
mizukosy <kazukoshy@gmail.com>
2020-09-17 18:06:47 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to:"items#index"
-  resources :items, only: :index
-  # resources :users, only: :create
+  resources :items, only: :index 
+  resources :users, only: :create
 end
