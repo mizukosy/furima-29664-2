4569e8b5ce7c0d195bfff04907dec8e14d84344e
mizukosy <kazukoshy@gmail.com>
2020-09-18 15:46:30 +0900

    パスワードカラムの削除

--- db/migrate/20200917024148_add_devise_to_users.rb
+++ db/migrate/20200917024148_add_devise_to_users.rb
@@ -6,7 +6,6 @@ class AddDeviseToUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       ## Database authenticatable
       t.string :nickname,           null: false
-      t.string :password,           null: false
       t.string :encrypted_password, null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :family_name,        null: false
