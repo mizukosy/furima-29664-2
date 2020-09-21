5f9cf7fef138cea5a93fafae0a407d0d75546169
mizukosy <kazukoshy@gmail.com>
2020-09-21 22:57:35 +0900

    item_controller_newの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,17 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index]
+  before_action :move_to_index, except: [:index, :new]
 
   def index
     
   end
 
+  def new
+    
+  end
+
 
   private
+  
   def move_to_index
     unless user_sign_in?
       redirect_to action: :index
