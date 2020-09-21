de44edb0faacf955864930e6bd7a3c349a0fbf79
mizukosy <kazukoshy@gmail.com>
2020-09-21 12:30:05 +0900

    Rubcop修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,12 @@
 class Item < ApplicationRecord
-  
   with_options presence: true do
-    validates :name,
-    validates :text,
-    validates :category_id,
-    validates :status_id,
-    validates :price,
-    validates :delivery_charger_id,
-    validates :delivery_area_id,
+    validates :name
+    validates :text
+    validates :category_id
+    validates :status_id
+    validates :price
+    validates :delivery_charger_id
+    validates :delivery_area_id
   end
 
   belongs_to :user
