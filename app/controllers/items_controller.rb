5008a02faf855c75eee0d5000901dbc298b05dbb
mizukosy <kazukoshy@gmail.com>
2020-09-21 16:04:32 +0900

    redirectionの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,11 +2,15 @@ class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index]
 
   def index
+    
   end
+
 
   private
-
   def move_to_index
-    redirect_to action: :index unless user_sign_in?
+    unless user_sign_in?
+      redirect_to action: :index
+    end
   end
+
 end
