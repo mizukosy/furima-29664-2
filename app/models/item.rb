bb27e1a091329867ac04c3367d663a1966ae6892
mizukosy <kazukoshy@gmail.com>
2020-09-22 17:48:45 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,22 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+
+  belongs_to :user
+  has_one_attached :image
+
   with_options presence: true do
     validates :name
     validates :text
-    validates :category_id
-    validates :status_id
     validates :price
-    validates :delivery_charger_id
-    validates :delivery_area_id
-  end
+    validates :image
 
-  belongs_to :user
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :status_id
+      validates :delivery_chager_id
+      validates :delivery_area_id
+      validates :delivery_days_id
+    end
+  end
 end
