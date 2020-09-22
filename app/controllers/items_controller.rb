22040725de1dd23f68803e6cb9679666fcbedf02
mizukosy <kazukoshy@gmail.com>
2020-09-22 17:44:40 +0900

    items controller create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,27 +2,34 @@ class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :new]
 
   def index
-    # @item = Item.new(item_params)
+    # @item = Item.all
   end
 
   def new
-    @category = Item.new
+    @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to items_path(@item)
+    else
+      render :new
+    end
+  end
 
   private
 
   def move_to_index
-    unless user_sign_in?
+    unless user_signed_in?
       redirect_to action: :index
     end
   end
 
   def item_params
-    # params.require(:item).permit(
-    #   :name, :text, :genre_id, :status_id, :price,
-    #   :delivery_charger_id, :delivery_area_id, :delivery_days_id, :user
-    # )
+    params.require(:item).permit(
+      :name, :text, :category_id, :status_id, :price, :image,
+      :delivery_chager_id, :delivery_area_id, :delivery_days_id, :user
+    ).merge( user_id:current_user.id)
   end
-
 end
