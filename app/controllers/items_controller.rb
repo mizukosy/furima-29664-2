fa11cf4dcaec57a1d717cbcec04416264f209c93
mizukosy <kazukoshy@gmail.com>
2020-09-26 17:09:07 +0900

    error handling

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,7 +37,11 @@ class ItemsController < ApplicationController
 
   def destroy
     @item.destroy
-    redirect_to root_path
+    if @item.destroy
+      redirect_to root_path
+    else
+      redirect_to item_path
+    end
   end
 
   private
