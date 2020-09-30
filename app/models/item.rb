e671b9768eba10c06e868a4633fd3989190e323d
mizukosy <kazukoshy@gmail.com>
2020-09-30 23:51:38 +0900

    prefectureへの変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,14 +2,13 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :status
-  belongs_to_active_hash :delivery_area
+  belongs_to_active_hash :prefecture
   belongs_to_active_hash :delivery_chager
   belongs_to_active_hash :delivery_days
 
   belongs_to :user
-  has_one :purchase
   has_one_attached :image
-  has_one :ship_address
+  has_one :purchase
 
   with_options presence: true do
     validates :name
@@ -21,7 +20,7 @@ class Item < ApplicationRecord
       validates :category_id
       validates :status_id
       validates :delivery_chager_id
-      validates :delivery_area_id
+      validates :prefecture_id
       validates :delivery_days_id
     end
   end
