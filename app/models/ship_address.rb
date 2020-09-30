0ab159de3e818d92edd5bc138903707358a477fa
mizukosy <kazukoshy@gmail.com>
2020-09-30 23:52:24 +0900

    バリデーションの修正

--- app/models/ship_address.rb
+++ app/models/ship_address.rb
@@ -1,14 +1,6 @@
 class ShipAddress < ApplicationRecord
-  belongs_to_active_hash :delivery_area
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+
   belongs_to :purchase
-
-  with_options presence: true do
-    validates :post_code,  format:{with:/\A\d{3}[-]\d{4}\z/}
-    validates :prefecture
-    validates :city
-    validates :block
-    validates :telephone
-    validates :purchase
-  end
-
 end
