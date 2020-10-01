53c74efe78c1cb1210089697deb5f28144d7915e
mizukosy <kazukoshy@gmail.com>
2020-10-01 19:01:53 +0900

    item_order モデルテストコード

---
+++ spec/factories/item_orders.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :item_order do
+    user_id       { 1 }
+    item_id       { 1 }
+    token         { "tok_abcdefghijk00000000000000000"}
+    post_code     { "111-1111" }
+    prefecture_id { Faker::Number.between(from: 2, to: 48)}
+    city          { Gimei.address.kanji}
+    block         { Gimei.town.to_s}
+    building      { "偽名ビル10A号" }
+    telephone     { Faker::PhoneNumber.subscriber_number(length:11)}
+  end
+end
