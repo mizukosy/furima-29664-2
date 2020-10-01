9e99c7c4a4c8998ff03cd29ca1ea801a46fd7d7a
mizukosy <kazukoshy@gmail.com>
2020-10-01 14:17:36 +0900

    purchase controllerの更新

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,17 +1,16 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!
+  before_action :select_item, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
     @order = ItemOrder.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order = ItemOrder.new(order_params)
     if @order.valid?
       pay_item
-      @order.create
+      @order.save
       return redirect_to root_path
     else
       render 'index'
@@ -21,8 +20,8 @@ class PurchasesController < ApplicationController
   private
 
   def order_params
-    params.require(:item_order).permit(:user_id, :item_id, :post_code, :prefecture_id, :city,
-                                       :block, :building, :telephone).merge(token: params[:token], user_id: current_user.id)
+    params.require(:item_order).permit(:post_code, :prefecture_id, :city, :block, :building, :telephone).merge(
+                                      token: params[:token], user_id: current_user.id, item_id: params[:item_id])
   end
 
   def pay_item
@@ -32,6 +31,10 @@ class PurchasesController < ApplicationController
       card: @order.token,
       currency: 'jpy'
     )
+  end
+
+  def select_item
+    @item = Item.find(params[:item_id])
   end
 
 end
