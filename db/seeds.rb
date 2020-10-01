18e99ca618d1b8f6aae0e5e5590e021e14a74a99
mizukosy <kazukoshy@gmail.com>
2020-10-01 14:18:25 +0900

    seedによるテストデータの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do |i|
+  item = Item.create!(name:'a',text:'aaa',category_id:'2',status_id:'2',price:'500', delivery_chager_id: 2, prefecture_id:'2', delivery_days_id:'2', user_id:'2')
+  item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+end
